6a72a9e1c859a4db6ce03e95f6997c738b40c497
gablon29 <gabriellondero40@gmail.com>
2024-06-17 08:10:55 -0300

    desarrollando la estructura del proyecto

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View, Button } from "react-native";
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        fontSize: 20,
+        justifyContent: "buttom",
+        alignItems: "center",
+      }}
+    >
+      <Text>Pomodoro</Text>
+    </View>
+  );
+};
+
+export default YourApp;
